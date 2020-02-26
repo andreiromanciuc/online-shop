06ae1c533ed973c90837b1306bd0e2dfee50403d
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-02-26 21:27:08 +0200

    added getProduct by Id (studied lambda's optional and custom exceptions)

---
+++ src/main/java/org/fasttrackit/onlineshop/exception/ResourceNotfoundException.java
@@ -0,0 +1,8 @@
+package org.fasttrackit.onlineshop.exception;
+
+public class ResourceNotfoundException extends RuntimeException {
+
+    public ResourceNotfoundException(String message) {
+        super(message);
+    }
+}
