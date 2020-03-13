f40d3d46eb048f504270a2329794ea3ede712651
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-03-13 19:23:12 +0200

    added get products method in product service

--- src/main/java/org/fasttrackit/onlineshop/domain/Product.java
+++ src/main/java/org/fasttrackit/onlineshop/domain/Product.java
@@ -2,6 +2,7 @@ package org.fasttrackit.onlineshop.domain;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
