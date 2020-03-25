687fda33ac6e4ebb03b1ece5856d528577cc9adf
Andrei Romanciuc <andrei.romanciuc@gmail.com>
2020-03-25 21:30:56 +0200

    overridden equals and hashCode for cart and product classes

--- src/main/java/org/fasttrackit/onlineshop/domain/Cart.java
+++ src/main/java/org/fasttrackit/onlineshop/domain/Cart.java
@@ -57,4 +57,18 @@ public class Cart {
         this.products = products;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cart cart = (Cart) o;
+
+        return id == cart.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return (int) (id ^ (id >>> 32));
+    }
 }
